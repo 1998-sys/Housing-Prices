4ddc7b2df310fb99bb48828eba7b35731a8cc78a
Matheus <matheus.bandeira74@gmail.com>
2025-07-15 22:58:12 -0300

    correlation map

--- src/utils.py
+++ src/utils.py
@@ -3,22 +3,23 @@ import numpy as np
 import seaborn as sns
 
 
-def relacao_variaveis(df, x, y, title=None):
+def iqr(df, coluna):
     """
-    Plota um scaterplot para visualizar a relação entre duas variáveis 
+    Detecação de outliers com iqr 
 
     Parâmetros:
     df (DataFrame): DataFrame contendo os dados.
-    x (str): Nome da variável para o eixo x.
-    y (str): nome da variável para o eixo y.
-    title (str, opcional): Título do gráfico. Se None, não será adicionado título.
+    coluna (str): Nome da variável para o cálculo.
 
     Retorna:
-    Axes: Objeto Axes do matplotlib com o gráfico plotado.
+    sup: limite superior.
+    inf: limite inferior
     """
-    ax = sns.scatterplot(data=df, x=x, y=y)
-    
-    if title:
-        ax.set_title(title)
-    
-    return ax
+    Q1 = df[coluna].quantile(0.25)
+    Q3 = df[coluna].quantile(0.75)
+
+    iqr = Q3 - Q1
+    inf= Q1 - 1.5 * iqr
+    sup= Q3 + 1.5 * iqr
+
+    return sup, inf
